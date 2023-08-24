6f97b1999480e44248ad1f702dec39e73f154d90
ox1david <bousi.david@gmail.com>
2023-08-24 22:04:42 +0200

    feat: implemented the algo

--- model/Blocks.ts
+++ model/Blocks.ts
@@ -2,11 +2,15 @@ abstract class Block {
     protected _xCoords: number[];
     protected _yCoords: number[];
     protected _color: string;
+    protected _xKernel: number[];
+    protected _yKernel: number[];
 
-    constructor(xCoords: number[], yCoords: number[], color: string) {
+    constructor(xCoords: number[], yCoords: number[], color: string, xkernel: number[], ykernel: number[]) {
         this._xCoords = xCoords;
         this._yCoords = yCoords;
         this._color = color;
+	this._xKernel = xkernel;
+	this._yKernel = ykernel;
     }
     get Color(): string {
         return this._color;
@@ -23,47 +27,59 @@ abstract class Block {
     set yCoords(newY: number[]) {
 	    this._yCoords = newY;
     }
+    get xKernel(): number[] {
+        return this._xKernel;
+    }
+    set xKernel(newKernel: number[]) {
+        this._xKernel = newKernel;
+    }
+    get yKernel(): number[] {
+        return this._yKernel;
+    }
+    set yKernel(newKernel: number[]) {
+        this._yKernel = newKernel;
+    }
 }
 
 class iBlock extends Block {
     constructor() {
-        super([4,5,6,7], [0,0,0,0], "rgb(0, 255, 255)") // Cyan
+        super([4,5,6,7], [0,0,0,0], "rgb(0, 255, 255)", [2,1,0,-1], [-1,0,1,2]) // Cyan
     }
 }
 
 class oBlock extends Block {
     constructor() { 
-    super([5,6,5,6], [0,0,1,1], "rgb(255, 255, 0)") // Yellow
+    super([5,6,5,6], [0,0,1,1], "rgb(255, 255, 0)", [0,0,0,0], [0,0,0,0]) // Yellow
     }
 }
 
 class tBlock extends Block {
     constructor() {
-        super([5,6,7,6], [0,0,0,1], "rgb(128, 0, 128)");  // Purple
+        super([5,6,7,6], [0,0,0,1], "rgb(128, 0, 128)", [1,0,-1,1], [-1,0,1,2]);  // Purple
     }
 }
 
 class sBlock extends Block {
     constructor() {
-        super([6,7,5,6], [0,0,1,1], "rgb(0, 255, 0)");  // Green
+        super([6,7,5,6], [0,0,1,1], "rgb(0, 255, 0)", [1,0,1,0], [-1,0,1,2]);  // Green
     }
 }
 
 class zBlock extends Block {
     constructor() {
-        super([5,6,6,7], [0,0,1,1], "rgb(255, 0, 0)");  // Red
+        super([5,6,6,7], [0,0,1,1], "rgb(255, 0, 0)", [2,1,0,-1], [0,1,0,1]);  // Red
     }
 }
 
 class jBlock extends Block {
     constructor() {
-        super([5,5,6,7], [0,1,1,1], "rgb(0, 0, 255)");  // Blue
+        super([5,5,6,7], [0,1,1,1], "rgb(0, 0, 255)", [2,1,0,-1], [0,-1,0,1]);  // Blue
     }
 }
 
 class lBlock extends Block {
     constructor() {
-        super([7,5,6,7], [0,1,1,1], "rgb(255, 165, 0)");  // Orange
+        super([7,5,6,7], [0,1,1,1], "rgb(255, 165, 0)", [1,0,-1,0], [-1,0,1,2]);  // Orange
     }
 }
 
--- model/Rotations.ts
+++ model/Rotations.ts
@@ -1,47 +1,18 @@
-interface Rotation {
-	rotate(board: Board, block: Block): Block;
-	adjustCoordinates(block: Block): Block;
+class Rotation {
+    public rotate(block: Block): Block {
+        const kernels = [
+            { x:  block._xKernel,  y:  block._yKernel },
+            { x: -block._yKernel,  y:  block._xKernel },
+            { x: -block._xKernel,  y: -block._yKernel },
+            { x:  block._yKernel,  y: -block._xKernel }
+        ];
 
-}
+        const kernel = kernels[block.angle];
+        block.xcoords = block.xcoords.map((x, i) => x + kernel.x[i]);
+        block.ycoords = block.ycoords.map((y, i) => y + kernel.y[i]);
 
-class NoRotation implements Rotation {
-	public rotate(board: Board, block: Block): Block {
-	return block;
-  }
-  	public adjustCoordinates(block: Block) {
-  	   return block; 
-  	}
-}
-
-class iRotation implements Rotation {
-	protected _kernel = [2, 1, 0, -1];
-
-	public rotate(board: Board, block: Block): Block {
-	block = this.adjustCoordinates(block);
-	block.angle = (block.angle + 1) % 4;
-	return block;
-  }
-
-  	public adjustCoordinates(block: Block): Block {
-    	if (block.angle === 2 || block.angle === 0) {
-      		block.xcoords = block.xcoords.map((x, i) => x + this._kernel[i]);
-		block.ycoords = block.ycoords.map((x, i) => x - this._kernel[i]);
-    	} else {
-      		block.xcoords = block.xcoords.map((x, i) => x - this._kernel[i]);
-		block.ycoords = block.ycoords.map((x, i) => x + this._kernel[i]);
-	
+        block.angle = (block.angle + 1) % 4;
+        return block;
     }
-    	return block;
-  }
 }
 
-class jRotation implements Rotation {
-
-}
-
-5 -> 6 -> 9    + 1 + 3 - 3 - 1   
-5 -> 7 -> 9    + 2 + 2 - 2 - 2
-6 -> 7 -> 8    + 1 + 1 - 1 - 1
-7 -> 7 -> 7    + 0 + 0 - 0 - 0
-       
-0,1,1,1
